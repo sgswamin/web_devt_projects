777ced06448c8876b1e117562f2cc9f748065677
Sruti <sgsofficial.system@gmail.com>
2024-04-03 20:16:15 -0700

    add js

---
+++ script.js
@@ -0,0 +1,18 @@
+function convert() {
+    const inputValue = document
+     .getElementById("userInput")
+     .value;
+    const unit = document
+     .getElementById('unit').value;
+    const milesToKm = unit ===
+     'milesToKm';
+    let result = 0;
+    if (milesToKm === true) {
+     result = inputValue * 1.60934;
+    } else {
+     result = inputValue / 1.60934;
+    }
+    document.getElementById(
+      'resultElement').innerHTML =
+     result;
+   }
